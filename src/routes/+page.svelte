1f9572a5b612c38605f966e4e6c07e784df890e5
Matt Nichols <mattnichols206@gmail.com>
2023-01-18 22:09:38 -0800

    Debug info & camera flip test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,69 @@
 <script lang="ts">
     import 'ress';
-
 	import { onMount } from 'svelte';
 
 	let videoElement: HTMLVideoElement;
+    let devices: MediaDeviceInfo[];
+    let errorString: string;
+    let selfieMode = true;
 
-	onMount(() => {
-		if (navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices
-				.getUserMedia({ video: true })
-				.then(function (stream) {
-					videoElement.srcObject = stream;
-				})
-				.catch((error) => {
-					console.log(error);
-				});
-		}
+	onMount(async () => {
+		startStream(selfieMode);
+        devices = await navigator.mediaDevices.enumerateDevices();
+        devices = devices.filter((device) => device.kind === 'videoinput');
 	});
+    
+    function startStream(frontFacing: boolean) {
+        if (!navigator.mediaDevices.getUserMedia) {
+            errorString = 'getUserMedia undefined';
+            return;
+        };
+
+        navigator.mediaDevices
+            .getUserMedia({
+                video: {
+                    facingMode: frontFacing ? 'user' : 'environment'
+                }
+            })
+            .then(function (stream) {
+                videoElement.srcObject = stream;
+            })
+            .catch((error) => {
+                errorString = error.message;
+            });
+    }
+
+    function clicked() {
+        selfieMode = !selfieMode;
+        startStream(selfieMode);
+    }
 </script>
 
 <svelte:head>
     <title>World Wide Webcam</title>
 </svelte:head>
 
-<div class="page-wrapper">
+<div class="video-overlay">
+    {#if errorString}
+        <div class="error">
+            {errorString}
+        </div>
+    {/if}
+
+    <button on:click={clicked}>
+        Flip!
+    </button>
+
+    <div class="device-list">
+        {#if devices}
+            {#each devices as device} 
+                <li>{device.label}</li>
+            {/each}
+        {/if}
+    </div>
+</div>
+
+<div class="video-wrapper">
     <!-- svelte-ignore a11y-media-has-caption -->
     <video class="video-element"
         autoplay={true}
@@ -34,14 +74,36 @@
 </div>
 
 <style>
-    .page-wrapper {
+    button {
+        background-color: white;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    .video-wrapper {
         width: 100vw;
         height: 100vh;
 
         display: flex;
         background-color: black;
     }
+
     .video-element {
-        flex-grow: 1;
+        width: 100%;
+        object-fit: cover;
+        transform: rotateY(180deg);
+    }
+
+    .video-overlay {
+        position: absolute;
+        width: 100%;
+        padding: 1em;
+        z-index: 1;
+
+        display: flex;
+        flex-direction: column;
+
+        background-color: rgba(0, 0, 0, 50%);
+        color: white;
     }
 </style>

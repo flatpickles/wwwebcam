cb95de5a3b358fbfb617d829c26db421ea46b19a
Matt Nichols <mattnichols206@gmail.com>
2023-03-08 09:44:06 -0800

    Fullscreen video element
    Perhaps this will improve resolution on larger screens.

--- src/lib/components/CameraCanvas.svelte
+++ src/lib/components/CameraCanvas.svelte
@@ -61,9 +61,10 @@
 <style>
 	.video-element {
 		position: absolute;
-		max-width: 100%;
-		max-height: 100%;
+		width: 100%;
+		height: 100%;
 		z-index: -1;
+        overflow: hidden;
 	}
 
 	.canvas-element {
